28cebc68a0cb95cb01bcf517a36a92f09c6885d2
Sami Munir <sami.munir2001@gmail.com>
2025-06-12 16:28:01 -0400

    creating server-side User model schema

---
+++ Projects/maravex/server/models/user.model.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    billingAddress: {
+      name: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+    },
+    shippingAddress: {
+      name: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+    },
+    orderHistory: {
+      type: [String],
+    },
+    savedProducts: {
+      type: [String],
+    },
+    paymentHistory: {
+      type: [String],
+    },
+    cardsOnFile: {
+      type: [String],
+    },
+    isSubscribed: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "customer"],
+      default: "customer",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
