cb01bf4ca0fd28c5f8ac8966115af56f50c798e7
Sami Munir <sami.munir2001@gmail.com>
2025-06-19 16:32:05 -0400

    implementing User model schema

---
+++ Client/top-tier-customs/server/models/user.model.js
@@ -0,0 +1,88 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+    },
+    location: {
+      city: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+    phone: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "customer"],
+      required: true,
+    },
+    billingAddress: {
+      name: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+    },
+    shippingAddress: {
+      name: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+    },
+    vehicles: {
+      type: [String],
+    },
+    savedProducts: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
