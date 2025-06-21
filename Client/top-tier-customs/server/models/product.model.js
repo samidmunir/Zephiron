29d751420534097a15ee193a2e6c6a1f00fe3ac7
Sami Munir <sami.munir2001@gmail.com>
2025-06-20 20:48:36 -0400

    implementing Product model schema

---
+++ Client/top-tier-customs/server/models/product.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    installable: {
+      type: Boolean,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    fits: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
