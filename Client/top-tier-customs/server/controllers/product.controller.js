58e8dba8e7cd1b173a1318e1abe27c8a2be1f373
Sami Munir <sami.munir2001@gmail.com>
2025-06-20 21:13:21 -0400

    implementing endpoints to getProduct(s)

--- Client/top-tier-customs/server/controllers/product.controller.js
+++ Client/top-tier-customs/server/controllers/product.controller.js
@@ -39,3 +39,47 @@ export const createProduct = async (req, res) => {
     });
   }
 };
+
+export const getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Failed to fetch product.",
+        error: "Invalid/unknown Product ID.",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Product found.", product: product });
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to fetch product.",
+      error: e.message,
+    });
+  }
+};
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Products fetched successfully.",
+        products: products,
+      });
+  } catch (e) {
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Failed to fetch products.",
+        error: e.message,
+      });
+  }
+};
--- Client/top-tier-customs/server/routers/product.router.js
+++ Client/top-tier-customs/server/routers/product.router.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createProduct } from "../controllers/product.controller.js";
+import {
+  createProduct,
+  getAllProducts,
+  getProduct,
+} from "../controllers/product.controller.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/create", createProduct);
+productRouter.get("/:id", getProduct);
+productRouter.get("/", getAllProducts);
 
 export default productRouter;
