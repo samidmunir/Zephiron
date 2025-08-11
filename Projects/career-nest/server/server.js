fd40e5c99623d4bf015612c67faa0c014891cea2
Sami Munir <sami.munir2001@gmail.com>
2025-08-11 11:48:49 -0400

    establishing MongoDB connection with server

--- Projects/career-nest/server/server.js
+++ Projects/career-nest/server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -13,4 +14,5 @@ app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`career-nest SERVER is running on http://localhost:${PORT}`);
+  connectDB();
 });
