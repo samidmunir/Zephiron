17207c7b6476544af99efaec7f3a024c22f34564
Sami Munir <sami.munir2001@gmail.com>
2025-08-12 07:59:14 -0400

    resetting frontend

--- Projects/career-nest/client/src/lib/axios.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true, // send cookies in every request
-});
-
-export default axiosInstance;
--- Projects/career-nest/client/src/pages/LoginPage.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-  return (
-    <main>
-      <h1>Login Page</h1>
-    </main>
-  );
-};
-
-export default LoginPage;
