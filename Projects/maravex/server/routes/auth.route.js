f9b38f6952cad27da71dc8e8057de6bfc7abc556
Sami Munir <sami.munir2001@gmail.com>
2025-08-27 23:53:42 -0400

    cleaning up codebase

--- Projects/maravex/server/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signup, login } from "../controllers/auth.controller.js";
-
-const authRouter = express.Router();
-
-authRouter.post("/signup", signup);
-authRouter.post("/login", login);
-
-export default authRouter;
