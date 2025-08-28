853d7c785a6a6e600a1ffe89bfa1175d456c0c95
Sami Munir <sami.munir2001@gmail.com>
2025-08-28 02:37:48 -0400

    implementing login() > auth.controller

--- Projects/career-nest/server/controllers/auth.controller.js
+++ Projects/career-nest/server/controllers/auth.controller.js
@@ -22,9 +22,7 @@ export const signup = async (req, res) => {
       lastName,
       email,
       password: hashedPassword,
-      city,
-      state,
-      country,
+      career,
     });
 
     const userRes = {
@@ -55,4 +53,59 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Login failed.",
+        error: `An account does not exist with email ${email}`,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Login failed.",
+        error: "Invalid credentials.",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    const userRes = {
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      dob: user.dob,
+      email: user.email,
+      phone: user.phone,
+      career: user.career,
+      billing: user.billing,
+      subscription: user.subscription,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful.",
+      token: token,
+      user: userRes,
+    });
+  } catch (error) {
+    console.log(`Error in login() <auth.controller>: ${error}`);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+      error: error.message,
+    });
+  }
+};
--- Projects/career-nest/server/routers/auth.router.js
+++ Projects/career-nest/server/routers/auth.router.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { login, signup } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
-authRouter.post("/login", () => {});
+authRouter.post("/login", login);
 
 export default authRouter;
